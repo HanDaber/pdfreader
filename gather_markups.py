86616f191421bbc8a905fca0a978f8c67aa8b139
Dan Haber <dh@Dans-MacBook-Air.local>
2021-04-14 01:46:20 -0700

    gather results

--- gather_markups.py
+++ gather_markups.py
@@ -1,7 +1,6 @@
 # main pipeline
 
 import sys, glob, os
-import pipeline as pipeline
 
 def main(*args):
     print("Gather markups from all jobs")
@@ -10,17 +9,21 @@ def main(*args):
     pdf_dir = args[0]
 
     print(f'PDF directory {pdf_dir}')
-    ret = os.popen(f'mkdir redlines')
+    ret = os.popen(f'mkdir -p export export/redlines export/artifacts')
     wat = ret.read()
     print(wat)
 
-    cmd_move = "echo 'GATHERING REDLINES'"
+    cmd_copy = "echo 'GATHERING REDLINES'"
 
     for redline in glob.iglob(f'artifacts/**/markup/*.pdf'):
         # print(f'markup: {redline}')
-        cmd_move += f' && mv {redline} redlines/'
+        cmd_copy += f' && cp {redline} export/redlines/'
 
-    ret = os.popen(cmd_move)
+    for page in glob.iglob(f'artifacts/**/markup/*.png'):
+        print(f'page: {page}')
+        cmd_copy += f' && mkdir -p export/artifacts/{page.split("/")[-3]} && cp {page} export/{page.replace("markup/", "").replace("_markup", "")}'
+
+    ret = os.popen(cmd_copy)
     wat = ret.read()
     print(wat)
 
--- gather_results.py
+++ gather_results.py
@@ -1,7 +1,18 @@
 # main pipeline
 
-import sys, glob, time
-import pipeline as pipeline
+import sys, glob, os
+from flask import Flask, render_template
+# from flask_restful import Resource, Api
+
+import JobsDB as Jobs
+import ResultsDB as Results
+
+app = Flask(__name__)
+# api = Api(app)
+
+@app.template_filter()
+def statusFormat(value):
+    return value.lower()
 
 def main(*args):
     print("Gather rersults from all jobs")
@@ -10,14 +21,32 @@ def main(*args):
     pdf_dir = args[0]
 
     print(f'PDF directory {pdf_dir}')
+    ret = os.popen(f'mkdir -p export export/redlines export/artifacts')
+    wat = ret.read()
+    print(wat)
 
-    with open(f'all_values.txt', 'w') as outfile:
-        for pdf_file in glob.iglob(f'{pdf_dir}/*.pdf'):
-            job_id = pdf_file.split(pdf_dir)[-1].split(".pdf")[0]
-            print(f'Getting results for job {job_id}')
+    for pdf_path in glob.iglob(f'{pdf_dir}/*.pdf'):
+        job_id = pdf_path.split("/")[-1].split(".")[0]
+        print(f'Job ID: {job_id}')
 
-            with open(f'{job_id}/results/values.txt', 'r') as finput:
-                outfile.write(finput.read()+"\n")
+        with app.app_context():
+            message = f"Jab {job_id}"
+            job = Jobs.find(job_id)
+            results = Results.find(job_id)
+            images = sorted(glob.iglob(f'export/artifacts/{job_id}/*.png'))
+            redline_path = pdf_path.replace("pdfs", "export/redlines").replace(".pdf", "_markup.pdf")
+            html = render_template('job.html', message=message, job=job, results=results, images=images, pdf_path=redline_path)
+
+            with open(f'export/{job_id}.html', 'w') as outfile:
+                outfile.write(html)
+
+    with app.app_context():
+        message = "Jabs"
+        jobs = Jobs.list_jobs()
+        html = render_template('index.html', message=message, jobs=jobs)
+        
+        with open(f'export/index.html', 'w') as outfile:
+            outfile.write(html)
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
